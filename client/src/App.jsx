aed8df9994f5ed26e383628bef3fb6fdd1afee9c
GONZALEZ-RODRIGUES <gonzalez.rodrigues@gmail.com>
2023-11-03 13:03:28 +0900

    client vite ok

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <p className="read-the-docs">
+        HELLO WORKD
+      </p>
+    </>
+  )
+}
+
+export default App
