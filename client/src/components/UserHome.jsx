b2c25406fcf75a891e2d91c255e6a1ce61f4334c
GONZALEZ-RODRIGUES <gonzalez.rodrigues@gmail.com>
2023-11-08 10:51:53 +0900

    feat: change card meet

--- client/src/components/UserHome.jsx
+++ client/src/components/UserHome.jsx
@@ -8,11 +8,9 @@ import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
 import { useLocation } from "react-router-dom";
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 import "../styles/UserHome.css";
-import { useState , useRef } from 'react';
+import { useState , useRef, useEffect } from 'react';
 import axios from 'axios';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
@@ -22,48 +20,24 @@ dayjs.extend(timezone);
 
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#EADBC8',
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    fontFamily: 'Poppins',
+    fontWeight: 400,
+    fontSize: 17,
   }));
-
-
-function getRandomNumber(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-/**
- * Mimic fetch with abort controller https://developer.mozilla.org/en-US/docs/Web/API/AbortController/abort
- * ⚠️ No IE11 support
- * 
- */
-function fakeFetch(date, { signal }) {
-
-  return new Promise((resolve, reject) => {
-    const timeout = setTimeout(() => {
-      const daysInMonth = date.daysInMonth();
-      const daysToHighlight = [1, 2, 3].map(() => getRandomNumber(1, daysInMonth));
-      
-      resolve({ daysToHighlight });
-    }, 500);
-
-    signal.onabort = () => {
-      clearTimeout(timeout);
-      reject(new DOMException('aborted', 'AbortError'));
-    };
-  });
-}
 
 const initialValue = dayjs();
 
 
 function ServerDay(props) {
-  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+  const { daysUserMeet = [], day, outsideCurrentMonth, ...other } = props;
 
   const isSelected =
-    !props.outsideCurrentMonth && highlightedDays.indexOf(props.day.date()) >= 0;
+    !props.outsideCurrentMonth && daysUserMeet.indexOf(props.day.date()) >= 0;
 
   return (
     <Badge
@@ -80,12 +54,11 @@ export default function DateCalendarServerRequest() {
   const [eventDate, setEventDate] = useState(dayjs());
   const location = useLocation();
   const userData = location.state;
-  const requestAbortController = React.useRef(null);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
-  const [meetHas, setMeetHas] = useState(false);
-
-  let daysUserMeet = [];
+  const requestAbortController = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMeet, setHasMeet] = useState(false);
+  const [daysUserMeet, setDaysUserMeet] = useState([])
+  // let daysUserMeet = [];
   const localInput = useRef("");
   const descriptionInput = useRef("");
   const timeInput = useRef("");
@@ -98,9 +71,8 @@ export default function DateCalendarServerRequest() {
         const response = await axios.get(url);
         if (response.status === 200) {
           const correctDay = response.data.map (m => dayjs(m.meet_date).utc().tz('Asia/Tokyo'))
-          daysUserMeet = correctDay.map(days => days.date())
-          console.log(daysUserMeet)
-            setHighlightedDays(daysUserMeet)
+          const onlyDays = correctDay.map(days => days.date())
+          setDaysUserMeet(onlyDays);
         } else {
           console.log("deu ruim");
         }
@@ -108,6 +80,20 @@ export default function DateCalendarServerRequest() {
         console.error(error);
     }
   
+  }
+  useEffect(() => {
+    const hasMeet = daysUserMeet.filter((dayHas) => dayHas === eventDate.$D);
+    if(hasMeet.length === 0) {
+      console.log("mudouu");
+      setHasMeet(false);
+    } else {
+      setHasMeet(true)
+    }
+  }, [daysUserMeet, eventDate]);
+
+  const handleMeet = () => {
+
+
   }
 
   const handleRequest = async (e) => {
@@ -124,11 +110,10 @@ export default function DateCalendarServerRequest() {
     const url = "http://localhost:5100/createmeet";
   
     try {
-      console.log(meetData.meet_date)
       const response = await axios.post(url, meetData);
       if (response.status === 200) {
         getMeets();
-       setMeetHas(true);
+       setHasMeet(true);
       } else {
         // setAlertOpen(true);
       }
@@ -137,108 +122,100 @@ export default function DateCalendarServerRequest() {
     }
   };
 
-
-  const fetchHighlightedDays = async (date) => {
+  const fetchHighlightedDays = async () => {
     const controller = new AbortController();
-    const daysWithMeet = await getMeets();
-    console.log(daysWithMeet)
-    fakeFetch(date, {
-      signal: controller.signal,
-    })
-      .then(({ daysToHighlight }) => {
-        console.log(daysToHighlight)
-        //setHighlightedDays(daysToHighlight);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        // ignore the error if it's caused by `controller.abort`
-        if (error.name !== 'AbortError') {
-          throw error;
-        }
-      });
-
+    getMeets();
+    setIsLoading(false);
     requestAbortController.current = controller;
   };
 
-  React.useEffect(() => {
-    fetchHighlightedDays(initialValue);
-    // abort request on unmount
+  useEffect(() => {
+    getMeets();
     return () => requestAbortController.current?.abort();
   }, []);
 
-  const handleMonthChange = (date) => {
+  const handleMonthChange = () => {
     if (requestAbortController.current) {
-      // make sure that you are aborting useless requests
-      // because it is possible to switch between months pretty quickly
       requestAbortController.current.abort();
     }
 
     setIsLoading(true);
-    setHighlightedDays([]);
-    fetchHighlightedDays(date);
+    setDaysUserMeet([]);
+    fetchHighlightedDays();
   };
-
+console.log(eventDate.$D)
+console.log(hasMeet)
   return (
 
     <div className='container-userhome'>
-    <div className="container-title-userhome">
+      <div className="container-title-userhome">
         <h1 className="title-userhome">GymMeet</h1>
-     </div>
-    <Box className="user-card" sx={{ flexGrow: 1 }}>
-              <Grid container spacing={2}>
-                  <Grid  item xs={12}>
-                      <Item >Hello, {userData.first_name}</Item>
-                  </Grid>
-                  <Grid  item xs={8}>
-                      <Item >
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar
-              defaultValue={initialValue}
-              value={eventDate}
-              onChange={(newEventDate) => setEventDate(newEventDate)}
-              loading={isLoading}
-              onMonthChange={handleMonthChange}
-              renderLoading={() => <DayCalendarSkeleton />}
-              slots={{
+      </div>
+      <div className="user-card">
+        <div className="user-info">
+          <h2 className='meet-card' >Hello, {userData.first_name}</h2>
+        </div>
+        <div className='container-calendar-meets'>
+          <div className="meets-info">
+             <div className='meets-cards' >Meets for you, {userData.first_name}</div>
+             <div className='meets-cards' >Meets for you, {userData.first_name}</div>
+             <div className='meets-cards' >Meets for you, {userData.first_name}</div>
+          </div>
+          <div className="date-calendar">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateCalendar
+                defaultValue={initialValue}
+                value={eventDate}
+                onChange={(newEventDate) => {
+                  setEventDate(newEventDate);
+                  handleMeet();
+                }}
+                loading={isLoading}
+                onMonthChange={handleMonthChange}
+                renderLoading={() => <DayCalendarSkeleton />}
+                slots={{
                   day: ServerDay,
-              }}
-              slotProps={{
+                }}
+                slotProps={{
                   day: {
-                      highlightedDays,
+                    daysUserMeet,
                   },
-              }} />
-      </LocalizationProvider>
-
-                      </Item>
-                  </Grid>
-                  <Grid item xs={4}>
-                      <Item >Meets for you, {userData.first_name}</Item>
-                      <Item >Meets for you, {userData.first_name}</Item>
-                      <Item >Meets for you, {userData.first_name}</Item>
-                  </Grid>
-                  <Grid item xs={12}>
-                      <Item >Lets make a GymMeet for {eventDate.format("MM/DD")}
-                      <br></br>
-                      <form className="form-meet" action="#" onSubmit={handleRequest}>
-                        <div>
-                        <label className="label-register" htmlFor="local">
-                              Gym:
-                            </label>
-                            <input
-                              className="input-login"
-                              type="text"
-                              name="local"
-                              placeholder="Anytime azabu"
-                              ref={localInput}
-                            />
-                        </div>
- 
-                            <div>
-                            <label className="label-register" htmlFor="goals">
-                              Hour:
-                            </label>
-                            <select  className="input-login" name="time" ref={timeInput}>
-                              <option value="00">00:00</option>
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+        </div>
+        {hasMeet ? (
+          <div>
+            <>
+            <div className="meet-card">
+              <h2 className='meet-card-title'>Your GymMeet for {eventDate.format("MM/DD")}</h2>
+            </div>
+          </>
+          </div>
+        ) : (
+          <>
+            <div className="meet-card">
+              <h2 className='meet-card-title'>Let's make a GymMeet for {eventDate.format("MM/DD")}</h2>
+              <form className="form-meet" action="#" onSubmit={handleRequest}>
+                <div>
+                  <label className="label-meet" htmlFor="local">
+                    Gym:
+                  </label>
+                  <input
+                    className="input-meet"
+                    type="text"
+                    name="local"
+                    placeholder="Anytime azabu"
+                    ref={localInput}
+                  />
+                </div>
+                <div>
+                  <label className="label-meet" htmlFor="goals">
+                    Hour:
+                  </label>
+                  <select className="input-meet" name="time" ref={timeInput}>
+                  <option value="00">00:00</option>
                               <option value="01">01:00</option>
                               <option value="02">02:00</option>
                               <option value="03">03:00</option>
@@ -262,27 +239,26 @@ export default function DateCalendarServerRequest() {
                               <option value="21">21:00</option>
                               <option value="22">22:00</option>
                               <option value="23">23:00</option>
-                            </select>
-                            </div>
-                            <div>
-                            <label className="label-register" htmlFor="description">
-                              Description (up to 150 characters):
-                            </label>
-                            <textarea
-                              // className="input-login"
-                              name="description"
-                              placeholder="Describe your workout plan for this day..."
-                              ref={descriptionInput}
-                              maxLength="150"
-                            ></textarea>
-                            </div>
-                            <button className="button-login">Meet!</button>
-                          </form>
-                      </Item>
-                  </Grid>
-              </Grid>
-          </Box>
-          
+                  </select>
+                </div>
+                <div>
+                  <label className="label-meet" htmlFor="description">
+                    Description (up to 150 characters):
+                  </label>
+                  <textarea
+                    name="description"
+                    placeholder="Describe your workout plan for this day..."
+                    ref={descriptionInput}
+                    maxLength="150"
+                    className="large-textarea"
+                  ></textarea>
+                </div>
+                <button className="button-login">Meet!</button>
+              </form>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
